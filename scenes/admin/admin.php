c930ea248e1841de1274ca343a1eee6f44eaecae
Diogo Familiar <afaculdadedodiogo@gmail.com>
2019-05-21 20:39:47 +0100

    adminadd

---
+++ scenes/admin/admin.php
@@ -0,0 +1,50 @@
+
+<?php /*
+session_start();
+if(isset($_SESSION['id_grupo'])=='7' || isset($_SESSION['id_utilizador'])){
+}else header('Location: ../../index.php');
+*/?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../css/custom.css">
+
+    <title>elVecino | Área administrativa</title>
+  </head>
+
+<body>
+
+	<?php
+	include __DIR__.'/../../headers/admin_header.php';
+	?>
+  
+  <h1 id="h1-centered">Bem-vindo</h1>
+	<div class="container" id="index_incidentes">
+    <h2 id="h1-centered">Incidentes</h2>
+    <a href="registo_incidente.php" class="btn btn-lg btn-change btn-block" role="button">Registar incidente</a>
+    <a href="estado_manutencoes.php" class="btn btn-lg btn-change btn-block" role="button">Estado das manutenções</a>
+  </div>
+  <?php 
+    if(isset($_COOKIE["alertaOk"])){
+      ?>
+      <SCRIpt>
+      alert("qualquercoisa");
+      </SCRIpt>
+      <?php
+    }
+    
+  ?>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>

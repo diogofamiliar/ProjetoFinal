5c0b93b3d9ebe6dac1e148ec8283cca2d9db57c0
AnaMoreira21602146 <a21602146@mso365.ulp.pt>
2019-05-27 15:48:18 +0100

    2

--- core/alter_user.php
+++ core/alter_user.php
@@ -2,31 +2,30 @@
 include "connect.php";
 session_start();
 
-if (isset ($_POST['nome'], $_POST['email'], $_POST['pw'], $_POST['data_nascimento'], $_POST['telefone1'], $_POST['telefone2'])) {
+if (isset ($_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['data_nascimento'], $_POST['telemovel'])) {
     $id_utilizador=$_SESSION['id_utilizador'];
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $data_nascimento = $_POST['data_nascimento'];
-    $pw =  $_POST['pw'];
-    $telefone1 = $_POST['telefone1'];
-    $telefone2 = $_POST['telefone2'];
+    $senha =  $_POST['senha'];
+    $telemovel = $_POST['telemovel'];
 
     $sql = "SELECT senha FROM utilizador WHERE id_utilizador='$id_utilizador'";
     $result=mysqli_query($conn,$sql);
     $row=mysqli_fetch_array($result);
     $hash=$row['senha'];
-    $compare=strcmp($pw,$hash); //compara o campo password do formulário com a hash guardada na bd
+    $compare=strcmp($senha,$hash); //compara o campo password do formulário com a hash guardada na bd
     echo "comparacao=".$compare;
 
     if ($compare==1) { //se a password for diferente
         //obter a nova hash
-        $hash=password_hash($pw, PASSWORD_DEFAULT);
-        $sql = "UPDATE utilizador SET nome='$nome', data_alteracao=now(), data_nascimento='$data_nascimento', email1='$email', telefone1='$telefone1', telefone2='$telefone2', senha='$hash' WHERE id_utilizador='$id_utilizador'";
+        $hash=password_hash($senha, PASSWORD_DEFAULT);
+        $sql = "UPDATE utilizador SET nome='$nome', data_criacao=now(), data_nascimento='$data_nascimento', email='$email', telemovel='$telemovel', senha='$hash' WHERE id_utilizador='$id_utilizador'";
         $result=mysqli_query($conn,$sql);
         header( "Location: ../scenes/cliente/cliente.php" );
 
     } else { //se a password for a mesma atualiza os outros campos menos a pass
-        $sql = "UPDATE utilizador SET nome='$nome', data_alteracao=now(), data_nascimento='$data_nascimento', email1='$email', telefone1='$telefone1', telefone2='$telefone2' WHERE id_utilizador='$id_utilizador'";
+        $sql = "UPDATE utilizador SET nome='$nome', data_criacao=now(), data_nascimento='$data_nascimento', email='$email', telemovel='$telemovel' WHERE id_utilizador='$id_utilizador'";
         $result=mysqli_query($conn,$sql);
 
         

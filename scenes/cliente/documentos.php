1bd9342be6fd3eade5d349ee2221c4810963e3ea
AnaMoreira21602146 <a21602146@mso365.ulp.pt>
2019-05-28 15:03:58 +0100

    documentos

--- scenes/cliente/documentos.php
+++ scenes/cliente/documentos.php
@@ -39,15 +39,6 @@ if(isset($_SESSION['id_grupo'])=='7' || isset($_SESSION['id_utilizador'])){
 	<div class="container" id="index_documentos">
     <h2 id="h1-centered">Documentos</h2>
   </div>
-
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- sweet alert -->
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <div class="container">
         <div class="table-responsive">  
             <table id="dados_documentos" class="table table-striped table-bordered">  
@@ -84,6 +75,7 @@ if(isset($_SESSION['id_grupo'])=='7' || isset($_SESSION['id_utilizador'])){
                 ?>  
             </tbody>  
             </table>  
+            <button type="submit" class="btn btn-primary btn-lg">Adicionar Documento</button>
         </div>  
     </div>  
     <?php
@@ -92,4 +84,7 @@ if(isset($_SESSION['id_grupo'])=='7' || isset($_SESSION['id_utilizador'])){
   ?>
 
 </body>
-
+</html>  
+ <script>  
+ $("#dados_documentos").bootgrid();  
+ </script>  

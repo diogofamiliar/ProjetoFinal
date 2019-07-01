9559d939f09bf42bc1bb1f0e36a07bed6b9d8ed9
Diogo Familiar <afaculdadedodiogo@gmail.com>
2019-07-02 00:26:54 +0100

    mensagens -> cx_entrada & arquivo

--- scenes/cliente/notificacoes/arquivar_notificacoes.php
+++ scenes/cliente/notificacoes/arquivar_notificacoes.php
@@ -5,19 +5,16 @@ if(isset($_SESSION['nome_grupo'])=='inquilino' || isset($_SESSION['nome_grupo'])
 ?>
 <?php
 include "../../../core/connect.php";
-/*
-if(isset($_SESSION['nome_grupo'])=='admin' && isset($_SESSION['id_utilizador'])){ */
     if(isset($_POST['id_mensagem'])){
         $id_mensagem=$_POST['id_mensagem'];
         foreach($id_mensagem as $valor){
             echo $valor;
-            $sql="DELETE FROM destinatario WHERE id_mensagem='$valor'";
+            $sql="UPDATE mensagem SET arquivada=2 WHERE id_mensagem='$valor'";
             $result=mysqli_query($conn,$sql);
 
         }
-    setcookie("notificacoes_eliminadas", "1", time()+(3), "/"); // o "/" disponibiliza a cookie para toda a plataforma
+    setcookie("notificacoes_arquivadas", "1", time()+(3), "/"); // o "/" disponibiliza a cookie para toda a plataforma
     header( "Location: notificacoes.php" );
     }
 
-/*}else header('Location: ../../../mensagens.php');    */
 ?>
--- scenes/cliente/notificacoes/notificacoes.php
+++ scenes/cliente/notificacoes/notificacoes.php
@@ -22,17 +22,17 @@ if(($_SESSION['nome_grupo'])=='cliente' || ($_SESSION['nome_grupo'])=='inquilino
     <title>Avisos</title>
     
     <script language="JavaScript" type="text/javascript">
-      function checkDelete() {
+      function checkArquivar() {
         var x = $('[name="id_mensagem[]"]:checked').length;
       if(x>0){ 
-        var confirmed = confirm("Pretende eliminar as mensagens?");
+        var confirmed = confirm("Pretende arquivar as mensagens?");
           if(confirmed){
             document.getElementById('form1').submit();
             return true;
           }
       }else{
         swal("Aviso!", 
-        "Selecione as notificações que pretende eliminar!", 
+        "Selecione as notificações que pretende arquivar!", 
         "error");
       return false;}
       }
@@ -54,55 +54,125 @@ if(($_SESSION['nome_grupo'])=='cliente' || ($_SESSION['nome_grupo'])=='inquilino
     <h1 id="h1-centered">Notificações e avisos</h1>
     <p>Lista de mensagens e avisos que recebeu.</p>
     <div class="card">
-      <div class="card-header d-flex justify-content-end">
-        <a class="btn btn-danger" href="javascript:{}" onclick="checkDelete()"><i class="fa fa-trash"></i> Eliminar</a>
+      <div class="card-header">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#cx_entrada">Cx entrada</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#arquivado">Arquivado</a>
+          </li>
+        </ul>
       </div>
-      <div class="card-body">
-        <form method="POST" id="form1" action="eliminar_notificacoes.php"> 
-          <table class="table table-hover">
-            <thead>
-              <tr>
-                <th><input type="checkbox" id="checkAll"/></th>
-                <th>Assunto</th>
-                <th>Mensagem</th>
-                <th>Data</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-            <?php
-              $sql = "SELECT mensagem.id_mensagem as id_mensagem, mensagem.assunto as assunto, mensagem.texto as mensagem, mensagem.data_criacao as data_criacao FROM mensagem INNER JOIN destinatario WHERE mensagem.id_mensagem=destinatario.id_mensagem AND destinatario.id_utilizador='$id_utilizador' ORDER BY mensagem.data_criacao DESC";
-              $resultset = $conn->query($sql);
-              if ($resultset->num_rows > 0) {
-              while($rows = mysqli_fetch_assoc($resultset)) {
-              ?>
-            <tr>
-                <td class="col-sm-1"><input type="checkbox" name="id_mensagem[]" value="<?php echo $rows['id_mensagem']; ?>" multiple></td>
-                <td><?php echo utf8_encode($rows["assunto"]); ?></td>
-                <td><?php echo utf8_encode($rows["mensagem"]); ?></td>
-                <td><?php echo utf8_encode($rows["data_criacao"]); ?></td>
-                <td><form method="POST" id="form2" action="mensagem_detalhe.php">
-                      <button form="form2" name="mensagem" class="btn btn-info" type="submit" value="<?php echo utf8_encode($rows["id_mensagem"]); ?>">Detalhes</button>
-                    </form>
-                </td>
-            </tr>
-            <?php
-              }}else{
-            ?>
-              <tr>
-                  <td></td>
-                  <td></td>
-                  <td>Não existem notificações ou avisos!</td>
-                  <td></td>
-                  <td></td>
-              </tr>
-                
-            <?php
-            }
-            ?>
-            </tbody>
-          </table>
-        </form>
+      <div>
+        <div class="tab-content">
+          <div id="cx_entrada" class="container tab-pane active">
+            <a class="btn btn-primary mx-2 my-2" href="javascript:{}" onclick="checkArquivar()"><i class="fa fa-trash"></i> Arquivar</a>  
+            <div class="card-body">
+              <form method="POST" id="form1" action="arquivar_notificacoes.php">
+                <div class="table-responsive">
+                  <table id="data" class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th><input type="checkbox" id="checkAll"/></th>
+                        <th>Assunto</th>
+                        <th>Mensagem</th>
+                        <th>Data</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                      $sql = "SELECT mensagem.id_mensagem as id_mensagem, mensagem.assunto as assunto, mensagem.texto as mensagem, mensagem.data_criacao as data_criacao, destinatario.lida as lida 
+                              FROM mensagem INNER JOIN destinatario 
+                              WHERE mensagem.id_mensagem=destinatario.id_mensagem AND destinatario.id_utilizador='$id_utilizador' AND mensagem.arquivada IS NULL ORDER BY mensagem.data_criacao DESC";
+                      $resultset = $conn->query($sql);
+                      if ($resultset->num_rows > 0) {
+                      while($rows = mysqli_fetch_assoc($resultset)) {
+                      ?>
+                    <tr class="<?php if ($rows['lida'] == "1"){ echo 'table-active';}else{ echo '';} ?>">
+                        <td class="col-sm-1"><input type="checkbox" name="id_mensagem[]" value="<?php echo $rows['id_mensagem']; ?>" multiple></td>
+                        <td><?php echo utf8_encode($rows["assunto"]); ?></td>
+                        <td><?php echo utf8_encode($rows["mensagem"]); ?></td>
+                        <td><?php echo utf8_encode($rows["data_criacao"]); ?></td>
+                        <td><form method="POST" id="form2" action="mensagem_detalhe.php">
+                              <button form="form2" name="mensagem" class="btn btn-info" type="submit" value="<?php echo utf8_encode($rows["id_mensagem"]); ?>">Detalhes</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                      }}else{
+                    ?>
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td>Não existem notificações ou avisos!</td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                        
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                  </table>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div id="arquivado" class="container tab-pane fade">
+            <div class="card-body">
+              <form method="POST" id="form1" action="arquivar_notificacoes.php">
+                <div class="table-responsive-sm">
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Assunto</th>
+                        <th class="col-sm-6">Mensagem</th>
+                        <th>Data</th>
+                        <th class="col-sm-auto"></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                      $sql = "SELECT mensagem.id_mensagem as id_mensagem, mensagem.assunto as assunto, mensagem.texto as mensagem, mensagem.data_criacao as data_criacao, destinatario.lida as lida 
+                      FROM mensagem INNER JOIN destinatario 
+                      WHERE mensagem.id_mensagem=destinatario.id_mensagem AND destinatario.id_utilizador='$id_utilizador' AND mensagem.arquivada IS NOT NULL ORDER BY mensagem.data_criacao DESC";
+                      $resultset = $conn->query($sql);
+                      if ($resultset->num_rows > 0) {
+                      while($rows = mysqli_fetch_assoc($resultset)) {
+                      ?>
+                    <tr class="<?php if ($rows['lida'] == "1"){ echo 'table-active';}else{ echo '';} ?>">
+                        <td><?php echo utf8_encode($rows["assunto"]); ?></td>
+                        <td><?php echo utf8_encode($rows["mensagem"]); ?></td>
+                        <td><?php echo utf8_encode($rows["data_criacao"]); ?></td>
+                        <td><form method="POST" id="form2" action="mensagem_detalhe.php">
+                              <button form="form2" name="mensagem" class="btn btn-info" type="submit" value="<?php echo utf8_encode($rows["id_mensagem"]); ?>">Detalhes</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                      }}else{
+                    ?>
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td>Não existem notificações ou avisos!</td>
+                          <td></td>
+                          <td></td>
+                      </tr>
+                        
+                    <?php
+                    }
+                    ?>
+                    </tbody>
+                  </table>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -129,7 +199,7 @@ if(($_SESSION['nome_grupo'])=='cliente' || ($_SESSION['nome_grupo'])=='inquilino
       "columnDefs": [
         { "width": "5%", "targets": 0 },
         { "width": "10%", "targets": 1 },
-        { "width": "40%", "targets": 2 },
+        { "width": "50%", "targets": 2 },
         { "width": "20%", "targets": 3 },
         { "width": "10%", "targets": 4 }
       ],
@@ -141,12 +211,12 @@ if(($_SESSION['nome_grupo'])=='cliente' || ($_SESSION['nome_grupo'])=='inquilino
   });
 </script>
 <?php
-  if(isset($_COOKIE["notificacoes_eliminadas"])){
+  if(isset($_COOKIE["notificacoes_arquivadas"])){
 ?>
       <script>
       swal({
             title: "Sucesso!",
-            text: "As mensagens foram eliminadas com sucesso!",
+            text: "As mensagens foram arquivadas com sucesso!",
             icon: "success",
             button: "Continuar",
       });

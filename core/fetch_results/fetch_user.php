5432a84b0cedb0c9858a23c505cb52240fbe945d
Diogo Familiar <afaculdadedodiogo@gmail.com>
2019-07-03 13:04:55 +0100

    admin criar_mensagem fix

--- core/fetch_results/fetch_user.php
+++ core/fetch_results/fetch_user.php
@@ -3,7 +3,11 @@
  if(isset($_POST["query"]))  
  {
       $output = '';  
-      $query = "SELECT id_utilizador, nome FROM utilizador WHERE nome LIKE '%".$_POST["query"]."%'";  
+      $query = "SELECT utilizador.id_utilizador as id_utilizador, utilizador.nome as nome, utilizador_grupo.id_grupo, grupo.nome as grupo_nome
+               FROM utilizador
+               INNER JOIN utilizador_grupo ON utilizador_grupo.id_utilizador=utilizador.id_utilizador
+               INNER JOIN grupo ON grupo.id_grupo=utilizador_grupo.id_grupo
+               WHERE grupo.nome!='admin' AND grupo.nome!='master' AND grupo.nome!='tecnico' AND utilizador.nome LIKE '%".$_POST["query"]."%'";  
       $result = mysqli_query($connect, $query);  
       $output = '<ul class="list-group">';  
       if(mysqli_num_rows($result) > 0)  

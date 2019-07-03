82d36e2f0d457b304203d96a172e454c63ec4711
AnaMoreira21602146 <a21602146@mso365.ulp.pt>
2019-07-03 14:19:00 +0100

    mensagens grupo

--- core/fetch_results/fetch_grupo.php
+++ core/fetch_results/fetch_grupo.php
@@ -3,7 +3,7 @@
  if(isset($_POST["query"]))  
  {
       $output = '';  
-      $query = "SELECT id_grupo, nome FROM grupo WHERE nome LIKE '%".$_POST["query"]."%'";  
+      $query = "SELECT id_grupo, nome FROM grupo WHERE grupo.nome!='admin' AND grupo.nome!='master' AND grupo.nome!='tecnico' AND nome LIKE '%".$_POST["query"]."%'";  
       $result = mysqli_query($connect, $query);  
       $output = '<ul class="list-group">';  
       if(mysqli_num_rows($result) > 0)  
